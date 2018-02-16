7b575d2a4860d98d26568b277d0a107f7f520068
jacob <jbutler8@terpmail.umd.edu>
2018-02-16 10:02:52 -0800

    functioning solution

---
+++ perfect_bits.rb
@@ -0,0 +1,49 @@
+def perfect_bits(a, b)
+  seen = Hash.new(0)
+  count = 0
+  i = a
+  while i <= b
+    binary = binaryify(i)
+    perfect, seen = perfect_bit?(binary, seen)
+    count += 1 if perfect
+    i += 1
+  end
+
+  count
+end
+
+def binaryify(a)
+  output = []
+  until a < 2
+    output.unshift(a % 2)
+    a = a/2
+  end
+
+  output.unshift(a).join("")
+end
+
+def perfect_bit?(a, seen = Hash.new(0))
+  return [false, seen] if a == "0"
+
+  ones = 0
+  a.chars.each { |n| ones += 1 if n == "1" }
+  return [true, seen] if ones == 1
+
+  if seen[ones] == -1 || seen[ones] == 1
+    return seen[ones] == -1 ? [false, seen] : [true, seen]
+  else
+
+    start = 1
+    last = seen[ones]
+    while start <= last
+      # if start ** 2 == ones
+      #   seen[ones] = 1
+      #   return [true, seen]
+      # end
+      # i += 1
+    end
+  end
+
+  seen[ones] = -1
+  [false, seen]
+end
